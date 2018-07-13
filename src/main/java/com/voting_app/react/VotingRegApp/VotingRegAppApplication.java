d4891c289b26c52c1eb5e6d3190554e8b1654796
Tommy Rogers <tsrjr@udel.edu>
2018-07-13 17:04:15 -0400

    test

---
+++ src/main/java/com/voting_app/react/VotingRegApp/VotingRegAppApplication.java
@@ -0,0 +1,12 @@
+package com.voting_app.react.VotingRegApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingRegAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingRegAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/voting_app/react/VotingRegApp/VotingRegAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.voting_app.react.VotingRegApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VotingRegAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
